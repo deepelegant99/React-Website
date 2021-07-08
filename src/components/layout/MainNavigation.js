98606d16e374cc1422ee442172c4e90980d07c89
Phillip Wong <deepelegant99@gmail.com>
2021-07-07 21:29:33 -0700

    Just testing

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,18 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function MainNavigation(){
-    return(
-        <header>
-            <h1>React Meetup</h1>
-            <nav>
-                <ul>
-                    <li><Link to='/'>All Meetups</Link></li>
-                    <li><Link to='/favorites'>Your Favorites</Link></li>
-                    <li><Link to='/new-meetup'>Start Your Own</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+function MainNavigation() {
+  return (
+    <header>
+      <h1>React Meetup</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Your Favorites</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Start Your Own</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
+
+//This is an experiment
