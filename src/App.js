a4b4b26e9e6012c2d4d1bfb560580da7a5f558d7
Phillip Wong <deepelegant99@gmail.com>
2021-07-09 04:42:02 -0700

    Added a layout for all the meetup events

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact={true}>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-      </Switch>
-    </div>
+    <Layout>
+      <div>
+        <Switch>
+          <Route path="/" exact={true}>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+        </Switch>
+      </div>
+    </Layout>
   );
 }
 
