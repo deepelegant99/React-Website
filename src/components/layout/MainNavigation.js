acd5020733bf2a19828c76fae75ca3dcbcfabc09
Henry Crossley <crossh2001@yahoo.com>
2021-07-09 02:21:00 -0700

    Adding More Components

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import {Link} from 'react-router-dom'
 
+import classes from './MainNavigation.module.css';
+
 function MainNavigation(){
     return(
-        <header>
-            <h1>React Meetup</h1>
+        <header className={classes.header}>
+            <h1 className={classes.logo}>React Meetup</h1>
             <nav>
                 <ul>
                     <li><Link to='/'>All Meetups</Link></li>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,6 +1,30 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_DATA =[
+	{
+		id: 'm1',
+		title: 'This is a first meetup',
+		image:	'https://honeysanime.com/wp-content/uploads/2016/06/Dead-or-Alive-5-wallpaper-660x500.jpg',
+		address: 'Meetupstreet 5, 12345 Meetup City',
+		description: 'This is first, amazing meetup which you definitely shold not miss.'
+	},
+	{
+		id: 'm2',
+		title: 'This is a second meetup',
+		image:	'https://www.guinnessworldrecords.com/Images/Tekken-7-main_tcm25-493320.jpg',
+		address: 'Meetupstreet 5, 12345 Meetup City',
+		description: 'This is second, amazing meetup which you definitely shold not miss.'
+	
+		
+	}
+];
+
 function AllMeetupsPage(){
     return(
+        <section>
         <div>All Meetups Page</div>
+        <MeetupList meetups={DUMMY_DATA} />
+        </section>
     )
 }
 
