6ca24c522305225f1f3f8962b6d782360629771e
Phillip Wong <deepelegant99@gmail.com>
2021-07-10 19:04:20 -0700

    New meetup events info is passed to the firebase server

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,28 @@
 import NewMeetupForm from "../components/meetup/NewMeetupForm";
 
+/*
+Data is posted on firebase using deepelegant99
+*/
+
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetup-react-ce1e2-default-rtdb.firebaseio.com//meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add Meetup Events</h1>
 
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
